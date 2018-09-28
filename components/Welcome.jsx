2d642cda270ca30117df78a6a3449a657520cf2a
Aurel Sziebert <aurel.sziebert@guidesmiths.com>
2018-09-28 12:59:14 +0200

    welcome phase

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { changeGamePhase } from '../store/actions';
+import { nextGamePhase } from '../store/actions';
 import { say } from '../lib/dealer';
 
 class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameTries: 0,
+    }
+  }
   componentDidMount() {
     say('first-welcome');
   }
   async componentWillReceiveProps(nextProps) {
     if (nextProps.lastSpeakResult.text !== null) {
       if (this.props.lastSpeakResult.text !== nextProps.lastSpeakResult.text) {
-        await say('greeting');
-        this.props.nextPhase();
+        if (this.state.nameTries === 0) {
+          await say('name-first');
+          this.setState({ nameTries: 1 });
+        } else {
+          await say('name-second1');
+          await say('name-second2');
+          this.props.nextPhase();
+        }
       }
     }
   }
@@ -30,7 +42,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  nextPhase: () => dispatch(changeGamePhase('takeBet')),
+  nextPhase: () => dispatch(nextGamePhase()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Welcome);
